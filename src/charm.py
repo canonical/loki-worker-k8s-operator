5d028c152444c6d92d3e3b4781ff8a8706968088
Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>
2024-06-14 16:36:40 -0300

    Update src/charm.py
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>

--- src/charm.py
+++ src/charm.py
@@ -4,13 +4,7 @@
 #
 # Learn more at: https://juju.is/docs/sdk
 
-"""Charm the service.
-
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-https://discourse.charmhub.io/t/4208
-"""
+"""Charm the service."""
 
 import json
 import logging

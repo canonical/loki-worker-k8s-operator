862ca93b54c7f02e892ea0d2a222d08a2a243a1a
Michael Thamm <mike.thamm@canonical.com>
2026-03-06 15:11:54 -0500

    fix: Split TF endpoints output to requires/provides

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,13 @@ output "app_name" {
   value = juju_application.loki_worker.name
 }
 
-output "endpoints" {
+output "provides" {
   value = {
-    # Requires
+  }
+}
+
+output "requires" {
+  value = {
     loki_cluster = "loki-cluster"
-    # Provides
   }
 }
